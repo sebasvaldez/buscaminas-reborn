5d8cbddf3a0261aa3740df953a853a67622279b8
sevasvaldez <marcelosebastianvaldez@gmail.com>
2022-01-22 00:55:30 -0300

    Corrigiendo funciones y agregando un poco de css

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-let columnas = 7;
-let filas = 3;
-
 const table = document.getElementById("table");
 
+const filas = document.getElementById("inputFila");
 
+const columnas = document.getElementById("inputCol");
 
-let tr2 = document.getElementById("tr2");
+const gTablero = document.getElementById("gTablero");
 
-
-let tr1 = document.getElementById("tr1");
-
+gTablero.addEventListener("click", () => {
+  generarTablero();
+});
 
 const generarTablero = () => {
+  for (let f = 0; f < filas.value; f++) {
+    let tierra= document.createElement("img")
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
 
-    
-    
-    for (let f = 0; f < filas; f++) {
-        let tr = document.createElement("tr")
-        table.appendChild(tr)
-        
-        let td = document.createElement("td")
-        
+    let td = document.createElement("td");
 
-        td.innerHTML = "Z"
+
+    //  td.innerHTML="2"
+    td.appendChild(tierra).src="./imagenes/tierra.jpg"
     
-        for (let c = 1; c < columnas; c++) {
-            
-            let td= document.createElement("td")
-            
-            tr.appendChild(td)
-            td.innerHTML = "Z"
-            
-        }
-        tr.appendChild(td)
+    
+
+    for (let c = 0; c < columnas.value-1; c++) {
+      let tierra= document.createElement("img")
+      let td = document.createElement("td");
+
+      tr.appendChild(td);
+      
+      td.appendChild(tierra).src="./imagenes/tierra.jpg"
+      
     }
     
-
+  }
 };
 
 
+const vaciarTablero= ()=>{
+    let tablero = []
+    for (let filas = 0; filas < filas; filas++) {
+
+        tablero.push([])
+        
+    }
+
+}
+
+const arrTablero = []
 
 
+for (let i = 0; i < columnas*filas; i++) {
 
 
-generarTablero();
-
+  
+  arrTablero.push()
+}
